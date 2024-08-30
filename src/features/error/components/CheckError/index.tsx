052962c4a855653c39f8e0d0adaba75144eeb506
hata0 <haataa0100@gmail.com>
2024-08-30 22:42:19 +0900

    chore: tanstack query を導入

--- src/features/error/components/CheckError/index.tsx
+++ src/features/error/components/CheckError/index.tsx
@@ -4,7 +4,7 @@ import { Custom401 } from "../401";
 import { Custom500 } from "../500";
 
 type Props = {
-  status: number;
+  status?: number;
 };
 export const CheckError = ({ status }: Props) => {
   if (status === 401) {
